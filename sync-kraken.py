e02d1ed1424df1f970fccab212657d7477b6bdf5
Your Name <ec2-user@ip-172-31-23-172.us-east-2.compute.internal>
2020-10-14 02:47:50 +0000

    .local

--- sync-kraken.py
+++ sync-kraken.py
@@ -3,8 +3,8 @@ from websocket import create_connection
 
 # Connect to WebSocket API and subscribe to trade feed for XBT/USD and XRP/USD
 ws = create_connection("wss://ws.kraken.com/")
-ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["XBT/USD","XRP/USD"]}')
 
+ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["ETH/XBT","XBT/USD","EOS/BTC","XLM/BTC","SC/BTC"]}')
 # Infinite loop waiting for WebSocket data
 while True:
         print(ws.recv())
