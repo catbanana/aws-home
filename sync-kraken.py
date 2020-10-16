9d8bf23e8c091d870d6ca45ab80e66ca1ec4b8f8
Your Name <ec2-user@ip-172-31-23-172.us-east-2.compute.internal>
2020-10-16 00:22:03 +0000

    yep

--- sync-kraken.py
+++ sync-kraken.py
@@ -1,10 +1,68 @@
 # Import WebSocket client library
+import numpy
+from notify_run import Notify 
+import re
+import websocket
 from websocket import create_connection
-
+import pandas as pd
 # Connect to WebSocket API and subscribe to trade feed for XBT/USD and XRP/USD
-ws = create_connection("wss://ws.kraken.com/")
+#ws = create_connection("wss://ws.kraken.com/")
 
-ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["ETH/XBT","XBT/USD","EOS/BTC","XLM/BTC","SC/BTC"]}')
+#ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["ETH/XBT","XBT/USD","EOS/BTC","XLM/BTC","SC/BTC"]}')
 # Infinite loop waiting for WebSocket data
-while True:
-        print(ws.recv())
+df = pd.DataFrame(columns=['event', 'subscription', 'pair'])
+[271,[["11467.10000","0.00993216","1602786626.143037","s","l",""]],"trade","XBT/USD"]
+
+def on_message(ws, message):
+    msg = json.loads(message)
+    print(msg)
+    global df
+                    # `ignore_index=True` has to be provided, otherwise you'll get
+                        # "Can only append a Series if ignore_index=True or if the Series has a name" errors
+    df = df.append(msg, ignore_index=True)
+
+def on_error(ws, error):
+    print(error)
+
+
+
+def on_close(ws):
+    print("### closed ###")
+
+
+def on_open(ws):
+    return
+
+
+if __name__ == "__main__":
+    ws = create_connection("wss://ws.kraken.com/")
+    ws.send('{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["ETH/XBT","XBT/USD","EOS/BTC","XLM/BTC","SC/BTC"]}')
+    while True:
+        msg = ws.recv()
+        if msg != '{"event":"heartbeat"}':
+            #print(msg)
+            k = 6
+            text = msg
+            text = text.split(",")
+            print(len(text))
+            text[:] = [s.replace('[', '') for s in text]
+            text[:] = [s.replace(']', '') for s in text]
+            text[:] = [s.replace('"', '') for s in text]
+            text[:] = [s.replace('{', '') for s in text]
+            text[:] = [s.replace('}', '') for s in text]
+            del text[k::k]
+            del text[k::k]
+            if 'trade' in text :
+                text.remove('trade')
+            print(text)
+            #if __name__ == "__main__":
+#    ws = websocket.WebSocketApp("wss://www.bitmex.com/realtime?subscribe=trade:XBTUSD", on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
+#    ws.run_forever()
+
+#while True:
+#        msg = ws.recv()
+#        if msg != '{"event":"heartbeat"}':
+#            print(msg)
+#            df = df.append(msg)
+            #notify = Notify() 
+            #notify.send(msg) 
